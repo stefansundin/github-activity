44f09c234d0dda159bef71d84ce9411a39a77534
Stefan Sundin <stefansundin@users.noreply.github.com>
2015-02-22 15:06:03 -0800

    Fixes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -24,6 +24,7 @@ before_fork do |server, worker|
     puts "Unicorn master intercepting TERM and sending myself QUIT instead"
     Process.kill "QUIT", Process.pid
   end
+  $redis.client.disconnect
 end
 
 after_fork do |server, worker|
